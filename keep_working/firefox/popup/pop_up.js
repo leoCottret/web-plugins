f0056c3a88c20bb67489b99cfad9c07e0f36bfa9
leoCottret <71428793+leoCottret@users.noreply.github.com>
2023-03-07 08:55:33 +0100

    add utility functions, add working limit addition [KW]

--- keep_working/firefox/popup/pop_up.js
+++ keep_working/firefox/popup/pop_up.js
@@ -1,8 +1,10 @@
-// TODO add more precise filter on hostname, otherwise params with &param="linkedin.com" will trigger the extention
+// just a shorter query selector
+function qS(selector) {
+	return document.querySelector(selector);
+}
 
-
-
-function createElement(parent, tag, text=null, cclass=null, id=null) {
+// add an html element to the dom
+function cE(parent, tag, text=null, cclass=null, id=null) {
 	let newElement = document.createElement(tag);
 	if (text) {
 		newElement.appendChild(document.createTextNode(text));
@@ -16,23 +18,52 @@ function createElement(parent, tag, text=null, cclass=null, id=null) {
 	return parent.appendChild(newElement);
 }
 
-timeWasteWebsites = [ { name: "linkedin.com", timeLimit: 15 }, { name: "youtube.com", timeLimit: 30 } ];
-
-let websiteListE = document.querySelector(".website-list");
-for (let w of timeWasteWebsites) {
-	let websiteRowE = createElement(websiteListE, "div", null, "website-row");
-	createElement(websiteRowE, "div", "-", "website-row-remove-button");
-	createElement(websiteRowE, "div", w.name, "website-row-text");
-	createElement(websiteRowE, "div", w.timeLimit, "website-row-text");
-
+// add new website time limit
+function addLimit() {
+	let websiteName = qS("#add-website-name-input").value;
+	let websiteTimeLimit = qS("#add-website-time-limit-input").value;
+	timeWasteWebsites.push({name: websiteName, timeLimit: websiteTimeLimit});
+	browser.storage.local.set({["keep_working"]: JSON.stringify(timeWasteWebsites)})
+	refreshTimeWasteWebsites();
 }
+
+function refreshTimeWasteWebsites() {
+	browser.storage.local.get({["keep_working"]: 1}).then((r) => {
+		// https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+		timeWasteWebsites = JSON.parse(r.keep_working);
+
+		let websiteListE = qS(".website-list");
+		websiteListE.innerHTML = "";
+		for (let w of timeWasteWebsites) {
+			let websiteRowE = cE(websiteListE, "div", null, "website-row");
+			cE(websiteRowE, "div", "-", "website-row-remove-button");
+			cE(websiteRowE, "div", w.name, "website-row-text");
+			cE(websiteRowE, "div", w.timeLimit, "website-row-text");
+		}
+	})
+}
+
+
+var timeWasteWebsites = null;
+refreshTimeWasteWebsites();
+
+// to avoid script-src csp error
+document.addEventListener('DOMContentLoaded', () => {
+    let addWebsiteE = qS("#add-website-button");
+    // onClick's logic below:
+    addWebsiteE.addEventListener('click', () => {
+        addLimit();
+    });
+});
+
+
 
 
 
 
 /*
 // get the toggle button
-let toggleExtensionCheckbox = document.querySelector("input[name=video-color-inverter-toggle]");
+let toggleExtensionCheckbox = qS("input[name=video-color-inverter-toggle]");
 
 // if the user clicked on the toggle button, modify the storage value accordingly, so video_color_inverter enable/disable the video color inversion
 toggleExtensionCheckbox.addEventListener('change', function() {
